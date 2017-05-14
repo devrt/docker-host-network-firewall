bc245678c4fb94301f62ac9b3b5672b57995c848
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2017-05-14 20:43:05 +0900

    use go template instead of jq

--- test.sh
+++ test.sh
@@ -9,12 +9,12 @@ echo "scan my host network with nmap from inside the container (assuming 24bit s
 docker run --rm nmap $MYIP/24
 
 echo "scan gateway ip for default network bridge"
-GWIP=`docker network inspect bridge | jq .[0].IPAM.Config[0].Gateway | sed 's/"//g'`
+GWIP=`docker network inspect --format='{{(index .IPAM.Config 0).Gateway}}' bridge`
 docker run --rm nmap $GWIP
 
 echo "scan gateway ip for newly created network bridge"
 NWNAME="bridge-network-for-testing-firewall"
 docker network create $NWNAME
-GWIP=`docker network inspect $NWNAME | jq .[0].IPAM.Config[0].Gateway | sed 's/"//g'`
+GWIP=`docker network inspect --format='{{(index .IPAM.Config 0).Gateway}}' $NWNAME`
 docker run --rm --net=$NWNAME nmap $GWIP
 docker network rm $NWNAME
